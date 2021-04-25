78f8bf17454db9e09f71a426acdf4852fd425789
Frederik Zilly <zilly@adminc.org>
2021-04-26 00:50:21 +0200

    Ajax Json PoC

--- searchengine/main.py
+++ searchengine/main.py
@@ -1,17 +1,45 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
+import json
 app = FastAPI()
 
+def getHTMLHeader(title='Hello World'):
+    returnString = ''
+    returnString=returnString+'<html><title>'+title+'</title><body>'
+    return returnString
 
-@app.get("/")
+def getHTMLFooter():
+    returnString = ''
+    returnString=returnString+'</body></html>'
+    return returnString
+
+
+@app.get("/", response_class=HTMLResponse)
 def read_root():
-    return {"Hello": "World"}
+    returnString = getHTMLHeader()+'test'+getHTMLFooter()
+    f = open('/home/zilly/datapipeline/searchengine/index.html', 'r')
+    returnString = f.read()
+    return returnString
+    #print(returnString)
+    #return {"Hello": "World"}
+
+@app.get("/books")
+def read_books():
+    f = open('/home/zilly/datapipeline/searchengine/books.json', 'r')
+    f2 = json.load(f)
+    #    returnString = f.read()
+    returnString = f2
+    print(returnString);
+    return returnString
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
-
+import uvicorn
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
